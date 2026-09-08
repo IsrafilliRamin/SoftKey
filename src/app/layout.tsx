6343704ce5816bbb53302711fecac9ccb1fc5da2
Israfilli Ramin <106513116+IsrafilliRamin@users.noreply.github.com>
2026-09-08 15:19:29 +0400

    fix:seo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,7 +19,7 @@ const inter = Inter({
   weight: ["400", "500", "600", "700"],
 });
 
-const siteUrl = process.env.NEXT_PUBLIC_SITE_URL ?? "https://softkey.az";
+const siteUrl = process.env.NEXT_PUBLIC_SITE_URL ?? "https://soft-key.vercel.app";
 
 export const metadata: Metadata = {
   metadataBase: new URL(siteUrl),
